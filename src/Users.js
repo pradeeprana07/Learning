826c8d2ff0cd2beab4ef56dbb2f9f1303b6a4ea1
PradeepSinghRana <131432380+pradeeprana07@users.noreply.github.com>
2023-07-17 10:10:27 +0530

    code update

---
+++ src/Users.js
@@ -0,0 +1,11 @@
+import './Users.css';
+
+function Users() {
+    return (
+      <div className="users">
+        <h1>hello this is from users.js</h1>
+      </div>
+    );
+  }
+
+  export default Users;
